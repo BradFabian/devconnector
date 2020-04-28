e0352cb046b07ac37536883e67b9997c941065e6
Brad <bradleofabian@gmail.com>
2020-04-28 16:43:44 -0400

    set up user reg

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
 
 // Bring in User Model
 const User = require("../../models/User");
@@ -27,14 +29,33 @@ router.post(
     try {
       let user = await User.findOne({ email });
       // See if user exists
-
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
       // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
 
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
       // Encrypt password
+      const salt = await bcrypt.genSalt(10);
 
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
       // Return Jsontoken
 
-      res.send("User route");
+      res.send("User registered");
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
