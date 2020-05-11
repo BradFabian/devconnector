66c1942eeaed82fb1f369eb977f71d21c8b13520
Brad <bradleofabian@gmail.com>
2020-05-11 13:43:51 -0400

    see if user exists

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,23 @@
-const jwt = require('jsonwebtoken');
-const config = require('config');
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
-module.exports = functions(req, res, next){
-    //get token from header
+module.exports = function (req, res, next) {
+  //get token from header
 
-    const token = req.header('x-auth-token');
+  const token = req.header("x-auth-token");
 
-    // check if not token
+  // check if not token
 
-    if(!token){
-        return res.status(401).json({ msg: 'No token, authorization denied'});
-    }
+  if (!token) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
 
-    //Verify token
-    try{
-        const decoded = jwt.verify(token, config.get('jwtSecert'));
-        req.user = decoded.user;
-        next();
-    }catch(err){
-        res.status(401).json({ msg: 'Token is not Valid'});
-    }
-}
+  //Verify token
+  try {
+    const decoded = jwt.verify(token, config.get("jwtSecert"));
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Token is not Valid" });
+  }
+};
