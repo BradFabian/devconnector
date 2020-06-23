c497ab97787e032711922c1e2ad45c96216a8ccb
Brad <bradleofabian@gmail.com>
2020-06-23 14:04:29 -0400

    fixed depreciate of use create index in mongoose and added an object in profiles for social media

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -41,6 +41,45 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body;
+    // Build profile object
+    const profileFields = {};
+
+    profileFields.user = req.user.id;
+    console.log(profileFields);
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(",").map((skill) => skill.trim());
+    }
+
+    // Build social object
+    profileFields.social = {};
+    if (youtube) profileFields.social.facebook = youtube;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (twitter) profileFields.social.facebook = twitter;
+    if (instagram) profileFields.social.facebook = instagram;
+    if (linkedin) profileFields.social.facebook = linkedin;
+
+    console.log(profileFields.skills);
+    res.send("Hello");
   }
 );
 
