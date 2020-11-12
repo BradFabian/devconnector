376f1af44c60dd5ed7ba59f8427848ea7ef71b73
Brad Fabian <bradleofabian@gmail.com>
2020-11-12 10:28:56 -0500

    added a login and logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layouts/Navbar";
 import Landing from "./components/layouts/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layouts/Alert";
+import {loadUser} from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
 
 //Redux
 import { Provider } from "react-redux";
@@ -12,7 +14,17 @@ import store from "./store";
 
 import "./App.css";
 
-const App = () => (
+if(localStorage.token){
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+
+  useEffect(() => {
+    store.dispatch(loadUser())
+  }, []);
+
+  return (
   <Provider store={store}>
     <Router>
       <Fragment>
@@ -28,6 +40,6 @@ const App = () => (
       </Fragment>
     </Router>
   </Provider>
-);
+)};
 
 export default App;
